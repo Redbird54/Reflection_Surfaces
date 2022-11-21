27436586b9feed3ebbd073f337a4dedd4044e94d
epeairs <eric.e.peairs@vanderbilt.edu>
2022-11-21 18:10:30 +0100

    Simplified input of objects

--- reflection_show.py
+++ reflection_show.py
@@ -5,35 +5,17 @@ raysOnly = True
 simpleView = False
 indivPlots = False
 
-# curv1 = Parabola(1,-2,-5,np.array([-2,-2]),np.array([1,2])) #a,b,c,initPoint,initDir
-# nextPoint,nextDir = curv1.reflect_vert(raysOnly, simpleView, indivPlots)	
+nextPoint,nextDir = np.array([0,0]),np.array([1,4])
 
-# curv2 = Parabola(1,2,4,np.array([-6,-2]),np.array([9,1])) 
-# nextPoint,nextDir = curv2.reflect_horiz(raysOnly, simpleView, indivPlots)
 
-# curv3 = Rotated_Parabola(1,-2,-5,np.array([-2,-2]),np.array([1,2])) #a,b,c,initPoint,initDir
-# nextPoint,nextDir = curv3.reflect(raysOnly, simpleView, indivPlots)	
+curv = Rotated_Ellipse(1,2,-5,3, nextPoint, nextDir) 
+nextPoint,nextDir = curv.reflect(raysOnly, simpleView, indivPlots)
 
-# curv3 = Linear(1,0,np.array([-1,-3]),np.array([1,2])) #a,b,initPoint,initDir
-# nextPoint,nextDir = curv3.reflect(raysOnly, simpleView, indivPlots)
+curv = Linear(-3,5,nextPoint,nextDir)
+nextPoint,nextDir = curv.reflect(raysOnly, simpleView, indivPlots)
 
-# curv4 = Hyperbola(1,2,1,3,np.array([1,-2]),np.array([1,4])) #a,b,h,k,initPoint,initDir
-# nextPoint,nextDir = curv4.reflect(raysOnly, simpleView, indivPlots)
-
-# curv4 = Rotated_Hyperbola(1,2,1,3,np.array([1,-2]),np.array([1,4]))
-# nextPoint,nextDir = curv4.reflect(raysOnly, simpleView, indivPlots)
-
-# curv5 = Ellipse(1,2,1,3, np.array([0,0]),np.array([1,4])) 
-# nextPoint,nextDir = curv5.reflect(raysOnly, simpleView, indivPlots)
-
-curv5 = Rotated_Ellipse(1,2,-1,3, np.array([0,0]),np.array([1,4])) 
-nextPoint,nextDir = curv5.reflect(raysOnly, simpleView, indivPlots)
-
-# curv3 = Linear(-3,5,nextPoint,nextDir) #a,b,initPoint,initDir
-# nextPoint,nextDir = curv3.reflect(raysOnly, simpleView, indivPlots)
-
-# curv4 = Rotated_Hyperbola(3,2,1,3,nextPoint,nextDir)
-# nextPoint,nextDir = curv4.reflect(raysOnly, simpleView, indivPlots)
+curv = Rotated_Hyperbola(3,2,1,3,nextPoint,nextDir)
+nextPoint,nextDir = curv.reflect(raysOnly, simpleView, indivPlots)
 
 
 if not(indivPlots):
@@ -45,5 +27,4 @@ if not(indivPlots):
     plt.xlim(-10, 10)
     plt.ylim(-10, 10)
     plt.gca().set_aspect('equal', adjustable='box')
-
     plt.show()
