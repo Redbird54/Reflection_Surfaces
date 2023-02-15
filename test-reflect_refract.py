ea25520a3592e11edc82e54ab680dbef388fe091
epeairs <eric.e.peairs@vanderbilt.edu>
2023-02-15 11:28:49 +0100

    Increased Readability

--- test-reflect_refract.py
+++ test-reflect_refract.py
@@ -16,7 +16,60 @@ outputs = queue.Queue()
 initialObjs = []
 objs = []
 
-nextPoint, nextRefl = np.array([-10,-40]),np.array([1,4])
+def get_valid_objs(initialObjs, objs, startPoint):
+    for obj in initialObjs:
+        if not(objs):
+            if any(abs(startPoint-obj.get_center()) > (boxsize*1.5)):
+                objs.append(obj)
+                if not(indivPlots):
+                    obj.show_curve()
+        else:
+            if any(abs(startPoint-obj.get_center()) > (boxsize*1.5)):
+                notOverlap = True
+                for x in range(len(objs)):
+                    centerDiff = [abs(a - b) >= (boxsize*3) for a, b in zip(obj.get_center(), objs[x].get_center())]
+                    if not(any(centerDiff)):
+                        notOverlap = False
+                if notOverlap:
+                    objs.append(obj)
+                    if not(indivPlots):
+                        obj.show_curve()
+    return objs
+
+def find_rays(outputs, objs):
+    for x in range(interactions):
+        if outputs.empty():
+            continue
+        currInfo = outputs.get()
+        distSmall = -1
+        currObj = Object()
+        for obj in objs:
+            dist = obj.get_distance(currInfo[0],currInfo[1])
+            if distSmall == -1 and dist > 0:
+                distSmall = dist
+                currObj = obj
+            elif dist == -1:
+                distSmall = distSmall
+            else:
+                if dist < distSmall:
+                    distSmall = dist
+                    currObj = obj
+
+        nextRays = currObj.output(distSmall, currInfo[0], currInfo[1], currInfo[2], currInfo[3], currInfo[4], indivPlots)
+        for nextRay in nextRays: 
+            if not(np.array_equal(nextRay[1], currInfo[1])):
+                outputs.put([nextRay[0], nextRay[1], currInfo[2], currInfo[3], nextRay[2]])
+            elif (np.array_equal(nextRay[2], currInfo[1])):
+                ##Show rays not interacting with any curves here
+                t = np.linspace(0, 30, 500)
+                plt.plot(nextRay[0][0] + t*nextRay[1][0], nextRay[0][1] + t*nextRay[1][1], 'orange')
+            if not(np.array_equal(nextRay[2], currInfo[1])):
+                if not(any(np.isnan(nextRay[2]))):
+                    outputs.put([nextRay[0], nextRay[2], currInfo[3], currInfo[2], nextRay[2]])
+    return outputs
+
+
+startPoint, startRefl = np.array([-10,-40]),np.array([1,4])
 # initialObjs.append(Polynomial3(5, 2, 3, -1, 7, 2, 0, -4, 1, -2, 0, 0,boxsize,"reflection"))
 # initialObjs.append(Polynomial2(5, 3, 7, 5, -1, 0, 0, 0,boxsize,"reflection"))
 initialObjs.append(Ellipse(1,2,1,8,boxsize,"reflection",math.pi/3))
@@ -26,58 +79,10 @@ initialObjs.append(Parabola(1,-15,13,boxsize,"refraction",5*math.pi/3))
 # initialObjs.append(Lens(-15,13,7,10,5,boxsize,9*math.pi/20))
 # initialObjs.append(Linear_Lens(-15,13,4,boxsize,18*math.pi/20))
 
-for obj in initialObjs:
-    if not(objs):
-        if any(abs(nextPoint-obj.get_center()) > (boxsize*1.5)):
-            objs.append(obj)
-            if not(indivPlots):
-                obj.show_curve()
-    else:
-        if any(abs(nextPoint-obj.get_center()) > (boxsize*1.5)):
-            notOverlap = True
-            for x in range(len(objs)):
-                centerDiff = [abs(a - b) >= (boxsize*3) for a, b in zip(obj.get_center(), objs[x].get_center())]
-                if not(any(centerDiff)):
-                    notOverlap = False
-            if notOverlap:
-                objs.append(obj)
-                if not(indivPlots):
-                    obj.show_curve()
 
-
-outputs.put([nextPoint,nextRefl,n1,n2,intensity])
-
-for x in range(interactions):
-    if outputs.empty():
-        continue
-    currInfo = outputs.get()
-    distSmall = -1
-    currObj = Object()
-    for obj in objs:
-        dist = obj.get_distance(currInfo[0],currInfo[1])
-        if distSmall == -1 and dist > 0:
-            distSmall = dist
-            currObj = obj
-        elif dist == -1:
-            distSmall = distSmall
-        else:
-            if dist < distSmall:
-                distSmall = dist
-                currObj = obj
-
-    nextRays = currObj.output(distSmall, currInfo[0], currInfo[1], currInfo[2], currInfo[3], currInfo[4], indivPlots)
-    for nextRay in nextRays: 
-        if not(np.array_equal(nextRay[1],currInfo[1])):
-            outputs.put([nextRay[0],nextRay[1],currInfo[2],currInfo[3],nextRay[2]])
-        elif (np.array_equal(nextRay[2],currInfo[1])):
-            ##Show rays not interacting with any curves here
-            t = np.linspace(0, 30, 500)
-            plt.plot(nextRay[0][0] + t*nextRay[1][0], nextRay[0][1] + t*nextRay[1][1],'orange')
-        if not(np.array_equal(nextRay[2],currInfo[1])):
-            if not(any(np.isnan(nextRay[2]))):
-                outputs.put([nextRay[0],nextRay[2],currInfo[3],currInfo[2],nextRay[2]])
-
-
+objs = get_valid_objs(initialObjs, objs, startPoint)
+outputs.put([startPoint, startRefl, n1, n2, intensity])
+outputs = find_rays(outputs, objs)
 
 if not(indivPlots):
     ##Show rays currently in the queue here
